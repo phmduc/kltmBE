8451e53cfd45b02ce6b55e59d2c4a5ddedf7d1eb
phmduc <phmduc1012@gmal.com>
2022-10-22 15:39:43 +0700

    newest

--- serverPhoneShop/controllers/productControl.js
+++ serverPhoneShop/controllers/productControl.js
@@ -1,88 +1,82 @@
 import asyncHandle from "express-async-handler";
 import Product from "../models/productModels.js";
 const productController = {
-    getAllProduct : asyncHandle(async (req, res) => {
-        try{
-            const products = await Product.find({});
-            res.json(products);
-        }catch(error){
-            throw new Error("Not Found List Product")
-        }
-           
-        }),
-    getSingleProduct : asyncHandle(async (req, res, next) => {
-        try{
-        const product = await Product.findById(req.params.id);
-        res.json(product)
-        }
-        catch(error){
-           throw new Error("Product Not Found")
-        }}
-        ),
-    getProductByCategory : asyncHandle(async (req, res, next) => {
-        try{
-        const product = await Product.find({category});
-        res.json(product)
-        }
-        catch(error){
-            throw new Error("Product Not Found")
-        }}
-        ),
-    addProduct : asyncHandle(async (req, res) => {
-       const { name , image, cate,  desc, price ,size } = req.body; 
-       const productExist = await Product.findOne({name})
-       if(productExist){
-           res.status(400);
-           throw new Error("Product name already exist");
-       }
-       else{
-       const product = new Product({
-           name,
-           image,
-           Cate:{idCate: cate.idCate,
-            nameCate: cate.nameCate, },
-           desc,
-           price,
-           size
-       });
-       console.log(product)
-       if(product){
-           const createdproduct = await product.save()
-           res.status(201).json(createdproduct);
-       }
-       else{
-           res.status(400);
-           throw new Error("Invalid Product");
-       }
-        }}),
-    updateProduct : asyncHandle(async (req, res) => {
-        const { name , image, desc, price , size } = req.body; 
-        const product = await Product.findById(req.params.id);
-        if(product){
-            product.name=name;
-            product.price =price;
-            product.desc =desc;
-            product.image =image;
-            product.size = size
-            const updatedProduct = await product.save();
-            res.json(updatedProduct);   
-        }
-        else{
-            res.status(400);
-            throw new Error("Invalid Product Data")
-        }
-        }),
-    deleteProduct : asyncHandle(async (req, res) => {
-        const product = await Product.findById(req.params.id);
-        if(product){
-            await product.deleteOne();
-            res.json("xoa thanh cong")
-        }
-        else{
-            res.status(400);
-            throw new Error("Not Found Product")
-        }
-        }),
-}
+  getAllProduct: asyncHandle(async (req, res) => {
+    try {
+      const products = await Product.find({});
+      res.json(products);
+    } catch (error) {
+      throw new Error("Not Found List Product");
+    }
+  }),
+  getSingleProduct: asyncHandle(async (req, res, next) => {
+    try {
+      const product = await Product.findById(req.params.id);
+      res.json(product);
+    } catch (error) {
+      throw new Error("Product Not Found");
+    }
+  }),
+  getProductByCategory: asyncHandle(async (req, res, next) => {
+    try {
+      const product = await Product.find({ category });
+      res.json(product);
+    } catch (error) {
+      throw new Error("Product Not Found");
+    }
+  }),
+  addProduct: asyncHandle(async (req, res) => {
+    const { name, image, cate, desc, price, size } = req.body;
+    const productExist = await Product.findOne({ name });
+    console.log(image);
+    if (productExist) {
+      res.status(400);
+      throw new Error("Product name already exist");
+    } else {
+      const product = new Product({
+        name,
+        image,
+        Cate: { idCate: cate.idCate, nameCate: cate.nameCate },
+        desc,
+        price,
+        size,
+      });
+      console.log(product);
+      if (product) {
+        const createdproduct = await product.save();
+        res.status(201).json(createdproduct);
+      } else {
+        res.status(400);
+        throw new Error("Invalid Product");
+      }
+    }
+  }),
+  updateProduct: asyncHandle(async (req, res) => {
+    const { name, image, desc, price, size } = req.body;
+    const product = await Product.findById(req.params.id);
+    if (product) {
+      product.name = name;
+      product.price = price;
+      product.desc = desc;
+      product.image = image;
+      product.size = size;
+      const updatedProduct = await product.save();
+      res.json(updatedProduct);
+    } else {
+      res.status(400);
+      throw new Error("Invalid Product Data");
+    }
+  }),
+  deleteProduct: asyncHandle(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+    if (product) {
+      await product.deleteOne();
+      res.json("xoa thanh cong");
+    } else {
+      res.status(400);
+      throw new Error("Not Found Product");
+    }
+  }),
+};
 
-export default productController
+export default productController;
--- serverPhoneShop/models/productModels.js
+++ serverPhoneShop/models/productModels.js
@@ -1,57 +1,58 @@
 import mongoose from "mongoose";
 
-const sizeSchema  = mongoose.Schema({
-    sizeId:{
-        type: String,
-        required: true,
-    },
-    count:{
-        type: Number,
-        default: 0,
-        required: true,
-    }
-})
-const imgSchema  = mongoose.Schema({
-    publicId:{
-        type: String,
-        required: true,
-    },
-    url:{
-        type: String,
-        required: true,
-    }
-})
+const sizeSchema = mongoose.Schema({
+  sizeId: {
+    type: String,
+    required: true,
+  },
+  count: {
+    type: Number,
+    default: 0,
+    required: true,
+  },
+});
+const imgSchema = mongoose.Schema({
+  publicId: {
+    type: String,
+    required: true,
+  },
+  url: {
+    type: String,
+    required: true,
+  },
+});
 
-const productSchema = mongoose.Schema({
-    name:{
-        type: String,
-        required: true
+const productSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
     },
-    image:[imgSchema],
-    desc:{
+    image: [imgSchema],
+    desc: {
+      type: String,
+    },
+    Cate: {
+      idCate: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Category",
+      },
+      nameCate: {
         type: String,
         required: true,
+      },
     },
-    Cate:{
-        idCate: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref:"Category"
-        },
-        nameCate: {
-            type: String,
-            required: true,}
-    }
-    ,
-    size:[sizeSchema],
-    price:{
-        type: Number,
-        required: true
+    size: [sizeSchema],
+    price: {
+      type: Number,
+      required: true,
     },
-},
-{
+  },
+  {
     timestamps: true,
-})
+  }
+);
 
-const Product = mongoose.model("Product",productSchema)
+const Product = mongoose.model("Product", productSchema);
 
-export default Product
+export default Product;
