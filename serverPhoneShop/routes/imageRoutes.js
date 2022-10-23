61acf377e567ac8613fa18d2599d9860ac3a72f2
phmduc <phmduc1012@gmal.com>
2022-10-23 18:02:44 +0700

    newest

--- serverPhoneShop/routes/imageRoutes.js
+++ serverPhoneShop/routes/imageRoutes.js
@@ -1,52 +1,50 @@
 import express from "express";
-import cloudinary from 'cloudinary'
+import cloudinary from "cloudinary";
 import asyncHandle from "express-async-handler";
-import fs from 'fs'
-import { CloudinaryStorage } from 'multer-storage-cloudinary';
-import multer from 'multer';
+import fs from "fs";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import multer from "multer";
 import path from "path";
-
 
 const imgRouter = express.Router();
 
 cloudinary.config({
-  cloud_name: 'dgw1cwtd1',
-  api_key: '128858954148247',
-  api_secret: 'GSMdk1zkQI5L4d6roTUysuvk9LM'
-
+  cloud_name: "dgw1cwtd1",
+  api_key: "128858954148247",
+  api_secret: "GSMdk1zkQI5L4d6roTUysuvk9LM",
 });
 
-
-
-imgRouter.post('/', async (req, res) => {
-  try{    
+imgRouter.post("/", async (req, res) => {
+  try {
     const file = req.body.file;
-    const promise = file.map( async (elem,index) => { 
-       return await cloudinary.v2.uploader.upload(elem, { folder:"test" }, (err, result)=>{
-        if(err) throw err;})
-      })
-    let images = await Promise.all(promise)
-    res.json(images)
-  }catch(err){
-    res.status(500).json("Oops, Error!")
-}
+    const promise = file.map(async (elem, index) => {
+      return await cloudinary.v2.uploader.upload(
+        elem,
+        { folder: "test" },
+        (err, result) => {
+          if (err) throw err;
+        }
+      );
+    });
+    let images = await Promise.all(promise);
+    res.json(images);
+  } catch (err) {
+    res.status(500).json("Oops, Error!");
+  }
 });
-imgRouter.post('/destroy', async (req,res)=>{
-  try{
-    const deletefile= req.body.publicId;
-    console.log(deletefile)
-    if(!deletefile) return res.status(400).json({ msg: 'no images selected'})
-    console.log(deletefile)
-    cloudinary.v2.uploader.destroy(deletefile, async(err, result)=>{
+imgRouter.post("/destroy", async (req, res) => {
+  try {
+    const deletefile = req.body.publicId;
+    console.log(deletefile);
+    if (!deletefile) return res.status(400).json({ msg: "no images selected" });
+    console.log(deletefile);
+    cloudinary.v2.uploader.destroy(deletefile, async (err, result) => {
       if (err) throw err;
-      res.json({msg: 'deleted image'})
-    })
-  }catch(err){
-    res.status(500).json("Oops, notfound")
+      res.json({ msg: "deleted image" });
+    });
+  } catch (err) {
+    res.status(500).json("Oops, notfound");
   }
 });
 
-export default imgRouter
-
-
-
+export default imgRouter;
