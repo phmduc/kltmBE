6072559fcc0f2ba8a1227cbf7c5d112d86c280d9
phmduc <phmduc1012@gmal.com>
2022-12-19 19:08:48 +0700

    newest

--- serverPhoneShop/data/user.js
+++ serverPhoneShop/data/user.js
@@ -1,17 +1,15 @@
-import bcrypt from 'bcryptjs'
+import bcrypt from "bcryptjs";
 export const users = [
-    {
-      name: "Admin",
-      email: "admin@gmail.com",
-      password: bcrypt.hashSync('123456',10),
-      isAdmin: true
-    },
-    {
-  
-        name: "User",
-        email: "User@gmail.com",
-        password: bcrypt.hashSync('123456',10),
-        isAdmin: false
-      },
-  ];
-  
+  {
+    name: "Admin",
+    email: "admin@gmail.com",
+    password: bcrypt.hashSync("123456", 10),
+    isAdmin: true,
+  },
+  {
+    name: "User",
+    email: "User@gmail.com",
+    password: bcrypt.hashSync("123456", 10),
+    isAdmin: false,
+  },
+];
